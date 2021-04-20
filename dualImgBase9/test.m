f879e7276bdc9debbd6ac7b87b4ccebf1051ab94
Justin Yang <a14506819@gmail.com>
2021-04-20 10:35:28 +0800

    改4個base9為一組

--- dualImgBase9/test.m
+++ dualImgBase9/test.m
@@ -2,7 +2,7 @@ clc
 clear
 
 cover_img_path = '..\images\mandrill.tif';
-maxbpp = 1.8;
+maxbpp = 2;
 % [ stego_img1_PSNR, stego_img2_PSNR, bpp ] = daul_base9( cover_img_path, maxbpp);
 
 output_array = daul_base9( cover_img_path, maxbpp);
